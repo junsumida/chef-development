0043301ac14d0f17a6cd314a39c1f41d0125590f
junsumida <sekitoh.rh@gmail.com>
2014-04-23 00:53:04 +0900

    berks cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-development'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-development'
+long_description 'Installs/Configures chef-development'
+version          '0.1.0'
+

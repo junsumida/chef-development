58b8c95f891c8926ec26fb31449b7d3ce8c11192
junsumida <sekitoh.rh@gmail.com>
2014-04-23 01:11:10 +0900

    add rbenv cookbook and gem installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# install rbenv and ruby
+include_recipe "rbenv::default"
+include_recipe "rbenv::ruby_build"
+rbenv_ruby "2.1.0"
+
+rbenv_gem %w{bundler berkshelf chef knife knife-solo} do
+  ruby_version "2.1.0"
+end
